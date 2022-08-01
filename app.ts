4f4dc36b1363a8bb84d1b0cecca4a46358b81872
jsz4n <3914641+jsz4n@users.noreply.github.com>
2022-08-01 16:13:52 +0200

    add dummy query in job

--- app.ts
+++ app.ts
@@ -1,5 +1,7 @@
 import { app, errorHandler, uuid } from 'mu';
 import { CronJob } from 'cron';
+import { querySudo as query, updateSudo as update } from "@lblod/mu-auth-sudo";
+import { prefixes } from "./prefixes";
 import {
   CRON_PATTERN,
 } from './env-config'
@@ -7,5 +9,19 @@ import {
 
 const pollingJob: CronJob = new CronJob( CRON_PATTERN, async () => {
   // poll data here
+  try{
+   const queryString = `
+   ${prefixes}
+   SELECT ?s WHERE  {
+     GRAPH ?g{
+       ?s a cpsv:PublicService.
+     }
+   }
+   `;
+    const response = await query(queryString);
+
+  } catch(e){
+    console.log(e);
+  }
 }, null, true);
 
---
+++ prefixes.ts
@@ -0,0 +1,12 @@
+export const prefixes = `
+PREFIX prov:  <http://www.w3.org/ns/prov#>
+PREFIX purl:  <http://purl.org/dc/terms/>
+PREFIX cpsv:  <http://purl.org/vocab/cpsv#>
+PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX adms:  <http://www.w3.org/ns/adms#>
+PREFIX vocab: <http://vocab.belgif.be/ns/publicservice#>
+PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+`;
+
+
