a81d0f557b847276b780eb18170209978aed3b86
jsz4n <3914641+jsz4n@users.noreply.github.com>
2022-08-01 15:16:35 +0200

    add empty cron job

--- app.ts
+++ app.ts
@@ -1,3 +1,11 @@
 import { app, errorHandler, uuid } from 'mu';
+import { CronJob } from 'cron';
+import {
+  CRON_PATTERN,
+} from './env-config'
 
+
+const pollingJob: CronJob = new CronJob( CRON_PATTERN, async () => {
+  // poll data here
+}, null, true);
 
---
+++ env-config.ts
@@ -0,0 +1,1 @@
+export const CRON_PATTERN: string = process.env.CRON_PATTERN || "*/1 * * * *"; //or every minute
