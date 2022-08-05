80112ee38061f758e43333653de95dc7aadcc43c
jsz4n <3914641+jsz4n@users.noreply.github.com>
2022-08-05 12:51:43 +0200

    correct ldes publisher environment

--- env-config.js
+++ env-config.js
@@ -1,4 +1,5 @@
 export const CRON_PATTERN = process.env.CRON_PATTERN || "*/1 * * * *"; //or every minute
 export const LDES_ENDPOINT = process.env.LDES_ENDPOINT;
 export const LDES_RELATION_PATH = process.env.LDES_RELATION_PATH;
-export const LDES_LDES_FRAGMENTER = process.env.LDES_LDES_FRAGMENTER;
+export const LDES_FRAGMENTER = process.env.LDES_FRAGMENTER;
+export const LDES_STREAM = process.env.LDES_STREAM;
