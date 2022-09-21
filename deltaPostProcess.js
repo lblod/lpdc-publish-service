91290d39f0342aa48663c918b212acc49463a673
jsz4n <3914641+jsz4n@users.noreply.github.com>
2022-09-21 08:29:43 +0200

    fix: use flatMap for extracting insertions

--- deltaPostProcess.js
+++ deltaPostProcess.js
@@ -2,7 +2,7 @@ import { isPublishedService, removePublishedStatus } from './queries';
 
 export async function processDelta(deltabody){
   //get insertions
-  const insertions = deltabody.map( x => x.inserts );
+  const insertions = deltabody.flatMap( x => x.inserts );
   for (let concept of insertions) {
     // check if status concept has been published
     const isPublished = isPublishedService( concept );
