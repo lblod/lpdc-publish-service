5ec385981a5778377bbeeb0342d8ce98620dee7b
jsz4n <3914641+jsz4n@users.noreply.github.com>
2022-10-07 16:39:05 +0200

    add deltabody filter

--- deltaPostProcess.js
+++ deltaPostProcess.js
@@ -1,8 +1,11 @@
 import { isPublishedService, removePublishedStatus } from './queries';
 
 export async function processDelta(deltabody){
+  const rdfType = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
+  const cpsvPublicService = "http://purl.org/vocab/cpsv#PublicService";
   //get insertions
-  const insertions = deltabody.flatMap( x => x.inserts );
+  const insertions = deltabody.flatMap( x => x.inserts )
+      .filter(x=> x.predicate.value == rdfType && x.object.value == cpsvPublicService);
   for (let concept of insertions) {
     // check if status concept has been published
     const isPublished = await isPublishedService( concept );
