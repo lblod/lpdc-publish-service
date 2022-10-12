94a2f657914e21214bbff63ac94b5aa39e2749a4
jsz4n <3914641+jsz4n@users.noreply.github.com>
2022-10-12 12:28:47 +0200

    change filter for correcting flow

--- deltaPostProcess.js
+++ deltaPostProcess.js
@@ -2,11 +2,11 @@ import { isPublishedService, removePublishedStatus } from './queries';
 
 export async function processDelta(deltabody){
   const admsStatus = "http://www.w3.org/ns/adms#status";
-  const sent_uri = "http://lblod.data.gift/concepts/9bd8d86d-bb10-4456-a84e-91e9507c374c";
+  const concept_uri = "http://lblod.data.gift/concepts/79a52da4-f491-4e2f-9374-89a13cde8ecd";
 
   //get insertions with type PublicService
   const insertions = deltabody.flatMap( x => x.inserts )
-      .filter(x=> x.predicate.value == admsStatus && x.object.value == sent_uri);
+      .filter(x=> x.predicate.value == admsStatus && x.object.value == concept_uri);
   for (let concept of insertions) {
     // check if status concept has been published
     const isPublished = await isPublishedService( concept );
