53e679eeebf2c4c2221f72d5f3a583efa9c9e578
Achraf Atauil <achrafatauil@hotmail.com>
2022-10-12 16:16:48 +0200

    add LDES_ENDPOINT_HEADER_PREFIX env

--- env-config.js
+++ env-config.js
@@ -1,3 +1,4 @@
 export const CRON_PATTERN = process.env.CRON_PATTERN || "*/1 * * * *"; //or every minute
 export const LDES_ENDPOINT = process.env.LDES_ENDPOINT;
 export const LDES_FOLDER = process.env.LDES_FOLDER || "";
+export const LDES_ENDPOINT_HEADER_PREFIX = 'LDES_ENDPOINT_HEADER_'
