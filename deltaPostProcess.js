6bc9f558770990aa4c1a23b03f6d90f4e5fec708
jsz4n <3914641+jsz4n@users.noreply.github.com>
2022-11-30 03:04:31 +0100

    add deletions to filtered bindings in a flattened structure

--- deltaPostProcess.js
+++ deltaPostProcess.js
@@ -4,10 +4,10 @@ export async function processDelta(concepts){
   const admsStatus = "http://www.w3.org/ns/adms#status";
   const concept_uri = "http://lblod.data.gift/concepts/79a52da4-f491-4e2f-9374-89a13cde8ecd";
 
-  //get insertions with type PublicService
-  const servicesInsertions = concepts.flatMap( x => x.inserts )
+  // get insertions and deletions with targeted status into a list of bindings
+  const deltasBindings = concepts.flatMap( x => [...x.inserts, x.deletes].flat() )
       .filter(x=> x.predicate.value == admsStatus && x.object.value == concept_uri);
-  for (let publicService of servicesInsertions) {
+  for (let publicService of deltaBindings) {
     // check if status concept has been published
     const isPublished = await isPublishedService( publicService );
     if (isPublished){
