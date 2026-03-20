42eb9aadd7609dba769539493ba9ec2e970d5dde
Kike Franssen <114907819+kikef98@users.noreply.github.com>
2026-03-20 13:44:48 +0100

    LPDC-1546 - Rewrite and clear up bizarre query (#21)
    Rewrite query and add Linting

--- app.js
+++ app.js
@@ -1,61 +1,75 @@
-import {app, errorHandler} from 'mu';
-import {CronJob} from 'cron';
-import bodyparser from 'body-parser';
+import { app, errorHandler } from "mu";
+import { CronJob } from "cron";
+import bodyparser from "body-parser";
 import {
   getPublicServiceDetails,
   getServicesToPublish,
   updateDatePublishedPublicService,
-  incrementRetryCounter
-} from './queries';
-import {putDataToIpdc} from './utils/putDataToIpdc';
-import {
-  CRON_PATTERN,
-  RETRY_COUNTER_LIMIT
-} from './env-config';
-import {clearPublicationErrors} from "./utils/publication-error";
+  incrementRetryCounter,
+} from "./queries";
+import { putDataToIpdc } from "./utils/putDataToIpdc";
+import { CRON_PATTERN, RETRY_COUNTER_LIMIT } from "./env-config";
+import { clearPublicationErrors } from "./utils/publication-error";
 
 app.use(bodyparser.json());
 
 let inProgress = false;
 
-new CronJob(CRON_PATTERN, async () => {
-  if (inProgress) {
-    console.log('Process already in progress');
-    return;
-  }
-  try {
-    inProgress = true;
-    const servicesToPublish = await getServicesToPublish();
-
-    console.log(`Found ${servicesToPublish.length} to publish`);
-    await clearPublicationErrors();
-    for (const service of servicesToPublish) {
-      try {
-        const subjectsAndData = await getPublicServiceDetails(service.publishedPublicService.value);
-
-        await putDataToIpdc(service.graph.value, service.publishedPublicService.value, subjectsAndData);
-        await updateDatePublishedPublicService(service.publishedPublicService.value, service.type.value);
-
-        console.log(`Successfully published ${service.publishedPublicService.value} of ${service.publicservice?.value}`);
-      } catch (e) {
-        await incrementRetryCounter(service.publishedPublicService.value);
-        const retriesLeft = RETRY_COUNTER_LIMIT - (service.publishRetryCount?.value ?? 0) - 1;
-        console.error(
-          `Could not publish ${service.publishedPublicService.value} of ${
-            service.publicservice?.value
-          }, ${retriesLeft} ${retriesLeft === 1 ? "retry" : "retries"} left${
-            retriesLeft === 0 ? ", giving up" : ""
-          }. Error: `,
-          e
-        );
-      }
+new CronJob(
+  CRON_PATTERN,
+  async () => {
+    if (inProgress) {
+      console.log("Process already in progress");
+      return;
     }
-  } catch (e) {
-    console.error('General error fetching data, retrying later');
-    console.log(e);
-  } finally {
-    inProgress = false;
-  }
-}, null, true);
+    try {
+      inProgress = true;
+      const servicesToPublish = await getServicesToPublish();
+
+      console.log(`Found ${servicesToPublish.length} to publish`);
+      await clearPublicationErrors();
+      for (const service of servicesToPublish) {
+        try {
+          const subjectsAndData = await getPublicServiceDetails(
+            service.publishedPublicService.value,
+          );
+
+          await putDataToIpdc(
+            service.graph.value,
+            service.publishedPublicService.value,
+            subjectsAndData,
+          );
+          await updateDatePublishedPublicService(
+            service.publishedPublicService.value,
+            service.type.value,
+          );
+
+          console.log(
+            `Successfully published ${service.publishedPublicService.value} of ${service.publicservice?.value}`,
+          );
+        } catch (e) {
+          await incrementRetryCounter(service.publishedPublicService.value);
+          const retriesLeft =
+            RETRY_COUNTER_LIMIT - (service.publishRetryCount?.value ?? 0) - 1;
+          console.error(
+            `Could not publish ${service.publishedPublicService.value} of ${
+              service.publicservice?.value
+            }, ${retriesLeft} ${retriesLeft === 1 ? "retry" : "retries"} left${
+              retriesLeft === 0 ? ", giving up" : ""
+            }. Error: `,
+            e,
+          );
+        }
+      }
+    } catch (e) {
+      console.error("General error fetching data, retrying later");
+      console.log(e);
+    } finally {
+      inProgress = false;
+    }
+  },
+  null,
+  true,
+);
 
 app.use(errorHandler);
